6d3c3485c626191abc1463d9f52dfd5f17636273
2groza <674072610@qq.com>
2022-07-29 08:24:07 +0800

    update results and re-run exps

--- RebuttalExperiments/main.py
+++ RebuttalExperiments/main.py
@@ -32,7 +32,7 @@ parser.add_argument("--policy", default="TD3")                  # Policy name (T
 parser.add_argument("--env", default="HalfCheetah-v3")          # OpenAI gym environment name
 parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
 parser.add_argument("--start_timesteps", default=10e3, type=int)  # Time steps initial random policy is used
-parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
+parser.add_argument("--eval_freq", default=25e3, type=int)       # How often (time steps) we evaluate
 parser.add_argument("--max_timesteps", default=2e5, type=int)   # Max time steps to run environment
 parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
 parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
